9cea868cc79fa381cc2347df8bcfbfdd434161c5
Zack Ankner <zackankner@gmail.com>
2023-06-04 08:19:06 +0000

    debugged script to get reference losses

--- do-remb-mi/scripts/data/build_reference_loss.py
+++ do-remb-mi/scripts/data/build_reference_loss.py
@@ -1,16 +1,21 @@
 import argparse
 
+from omegaconf import OmegaConf
 import torch
-from torch.utils.data import DataLoader, default_collate
+import torch.nn as nn
+from torch.utils.data import DataLoader
 from composer import State, Trainer, Callback
 from composer.callbacks import RuntimeEstimator
 from composer.loggers import Logger
+from composer.metrics.nlp import LanguageCrossEntropy
 import composer.utils.dist as dist
 from streaming import MDSWriter
+import transformers
 from transformers import AutoTokenizer
 
 from llmfoundry.data.text_data import (ConcatenatedSequenceCollatorWrapper,
                                        StreamingTextDataset)
+from llmfoundry.models.mpt import ComposerMPTCausalLM
 
 
 class ReferenceLossCallback(Callback):
@@ -19,43 +24,83 @@ class ReferenceLossCallback(Callback):
         super().__init__()
         streaming_writer = None
         if dist.get_global_rank() == 0:
-            columns = {"tokens": "bytes", "loss": "float32"}
+            columns = {"tokens": "bytes", "ref_losses": "bytes"}
             streaming_writer = MDSWriter(columns=columns,
                                          out=writer_path,
-                                         compression="ztsd")
+                                         compression="zstd")
         self.streaming_writer = streaming_writer
+        self.ref_loss_fn = nn.CrossEntropyLoss(ignore_index=-100,
+                                               reduction="none")
 
     def eval_after_forward(self, state: State, logger: Logger) -> None:
         ref_logits = state.outputs.logits
         targets = state.model.get_targets(state.batch)
         b, n_tokens, vocab_size = ref_logits.shape
 
-        losses = state.model.loss_fn(ref_logits.view(-1, vocab_size),
-                                     targets.view(-1))
-        losses = losses.view(b, n_tokens)
+        state.model.labels = targets  # So we can compute metrics
 
+        losses = self.ref_loss_fn(ref_logits.view(-1, vocab_size),
+                                  targets.view(-1))
+        losses = losses.view(
+            b, n_tokens)  # Might change from targets to batch input ids
+
+        all_losses = torch.vstack(dist.all_gather(losses))
+        all_tokens = torch.vstack(dist.all_gather(targets))
         if dist.get_global_rank() == 0:
-            all_losses = torch.vstack(dist.all_gather(losses))
-            all_tokens = torch.vstack(dist.all_gather(targets))
-
             for losses, tokens in zip(all_losses, all_tokens):
-                byte_losses = losses.numpy().tobytes()
-                byte_tokens = tokens.numpy().tobytes()
+                byte_losses = losses.cpu().numpy().tobytes()
+                byte_tokens = tokens.cpu().numpy().tobytes()
                 self.streaming_writer.write({
                     "tokens": byte_tokens,
-                    "ref_loss": byte_losses
+                    "ref_losses": byte_losses
                 })
         dist.barrier()
 
     def eval_end(self, state: State, logger: Logger) -> None:
-        self.streaming_writer.finish()
+        if dist.get_global_rank() == 0:
+            self.streaming_writer.finish()
+        dist.barrier()
+
+
+def build_model(model_size, tokenizer, max_seq_len):
+    if model_size == "125M":
+        model_cfg = OmegaConf.create({
+            "name": "mpt_causal_lm",
+            "init_device": "meta",
+            "d_model": 768,
+            "n_heads": 12,
+            "n_layers": 12,
+            "expansion_ratio": 4,
+            "max_seq_len": max_seq_len,
+            "vocab_size": 50432,
+            "no_bias": True,
+            "attn_config": {
+                "alibi": True,
+                "attn_impl": "triton",
+                "clip_qkv": 6,
+                "attn_uses_sequence_id": True
+            }
+        })
+        fsdp_cfg = OmegaConf.create({
+            "activation_checkpointing": False,
+            "activation_checkpointing_reentrant": False,
+            "activation_cpu_offload": False,
+            "limit_all_gathers": True,
+            "mixed_precision": "PURE",
+            "sharding_strategy": "FULL_SHARD",
+            "state_dict_type": "full",
+            "verbose": False
+        })
+        return ComposerMPTCausalLM(model_cfg, tokenizer), fsdp_cfg
+    else:
+        raise ValueError(f"Unkown model size: {model_size}")
 
 
 def main(args):
-    device_batch_size = args.batch_size / dist.get_world_size()
-    assert args.batch_size % device_batch_size != 0, "Batch size must be divisible by the number of devices"
+    device_batch_size = int(args.batch_size / dist.get_world_size())
+    assert args.batch_size % device_batch_size == 0, "Batch size must be divisible by the number of devices"
 
-    for domain_id in args.num_domains:
+    for domain_id in range(args.num_domains):
         print(f"Starting to build losses for domain {domain_id}...")
 
         streaming_writer_path = f"/tmp/domains/domain-{domain_id}/train"
@@ -67,13 +112,15 @@ def main(args):
             remote=
             f"oci://mosaicml-internal-doremi/pile/pre-concat/gpt-neox-20b-seqlen-2048/data-sources/domain-{domain_id}",
             local=f"/tmp/streaming/domain-{domain_id}",
-            split="train",
+            split="val",
             batch_size=device_batch_size,
             shuffle=False,
             shuffle_algo='py1b',
         )
+        lm_collate_fn = transformers.DataCollatorForLanguageModeling(
+            tokenizer=dataset.tokenizer, mlm=False)
         collate_fn = ConcatenatedSequenceCollatorWrapper(
-            base_collator=default_collate,
+            base_collator=lm_collate_fn,
             eos_token_id=args.eos_token_id,
             bos_token_id=None)  #Fix
         dataloader = DataLoader(
@@ -87,17 +134,23 @@ def main(args):
             persistent_workers=True,
         )
 
-        model = None
+        model, fsdp_cfg = build_model(args.ref_model_size,
+                                      tokenizer=tokenizer,
+                                      max_seq_len=args.max_seq_len)
+        model.use_logits = False  # So that full ModellingOutputs passed to callback
+        model.val_metrics = {
+            LanguageCrossEntropy.__name__: LanguageCrossEntropy()
+        }  # Only metric we care about
 
         ref_loss_callback = ReferenceLossCallback(
             writer_path=streaming_writer_path)
-        runtime_callback = RuntimeEstimator()
 
         trainer = Trainer(model=model,
                           eval_dataloader=dataloader,
                           progress_bar=True,
-                          load_path=args.reference_model_ckpt,
-                          callbacks=[ref_loss_callback, runtime_callback])
+                          load_path=args.ref_model_ckpt,
+                          fsdp_config=fsdp_cfg,
+                          callbacks=[ref_loss_callback])
         trainer.eval()
         trainer.close()
 
@@ -106,12 +159,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch-size", type=int, default=256)
     parser.add_argument("--num-domains", type=int, required=True)
-    parser.add_argument("--reference-model-ckpt", type=str, required=True)
+    parser.add_argument("--ref-model-size",
+                        type=str,
+                        choices=["125M"],
+                        required=True)
+    parser.add_argument("--ref-model-ckpt", type=str, required=True)
     parser.add_argument("--tokenizer",
                         type=str,
                         default="EleutherAI/gpt-neox-20b")
     parser.add_argument("--eos-token-id", type=int, default=0)
-    parser.add_argument("--max-length", type=int, default=2048)
+    parser.add_argument("--max-seq-len", type=int, default=2048)
     args = parser.parse_args()
 
     dist.initialize_dist(device="gpu")
