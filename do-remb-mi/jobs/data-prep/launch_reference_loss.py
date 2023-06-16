c16c023867375029c5c85439376046da64622ac7
Zack Ankner <zackankner@gmail.com>
2023-06-15 23:21:04 -0700

    Refactor launch_reference_loss.py script

--- do-remb-mi/jobs/data-prep/launch_reference_loss.py
+++ do-remb-mi/jobs/data-prep/launch_reference_loss.py
@@ -6,12 +6,17 @@ from mcli import RunConfig, create_run
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--num-domains", type=int, required=True)
+    parser.add_argument("--model-size", type=str, default="125M")
+    parser.add_argument("--split", type=str, default="train")
     parser.add_argument("--subset-domains", nargs="+", type=int, default=[])
+    parser.add_argument("--remote-base", type=str, required=True)
+    parser.add_argument("--ref-model-ckpt", type=str, required=True)
+    parser.add_argument("--upload-dir", type=str, required=True)
     parser.add_argument("--mode", type=str, default="parallel")
     args = parser.parse_args()
 
-    if len(args.subset_domains) == 0:
-        args.subset_domains = [*range * (args.num_domains)]
+    if not args.subset_domains or len(args.subset_domains) == 0:
+        args.subset_domains = [*range(args.num_domains)]
 
     if args.mode == "parallel":
         for domain_id in range(args.num_domains):
@@ -21,11 +26,20 @@ if __name__ == "__main__":
                 f"do-remb-mi/jobs/data-prep/yamls/build_reference_loss.yaml")
 
             base_run.name = f"ref-loss-domain-{domain_id}"
-            base_run.run_name = f"ref-loss-domain-{domain_id}"
+            base_run.run_name = f"ref-loss-domain-{domain_id}-{args.split}"
 
             base_run.command = re.sub(r'{SUBSET_DOMAINS}', str(domain_id),
                                       base_run.command)
             base_run.command = re.sub(r'{NUM_DOMAINS}', str(args.num_domains),
+                                      base_run.command)
+            base_run.command = re.sub(r'{MODEL_SIZE}', str(args.model_size),
+                                      base_run.command)
+            base_run.command = re.sub(r'{SPLIT}', args.split, base_run.command)
+            base_run.command = re.sub(r'{REF_MODEL_CKPT}', args.ref_model_ckpt,
+                                      base_run.command)
+            base_run.command = re.sub(r'{REMOTE_BASE}', args.remote_base,
+                                      base_run.command)
+            base_run.command = re.sub(r'{UPLOAD_DIR}', args.upload_dir,
                                       base_run.command)
 
             launched_run = create_run(base_run)
@@ -41,7 +55,16 @@ if __name__ == "__main__":
 
         base_run.command = re.sub(r'--subset-domains {SUBSET_DOMAINS} ', '',
                                   base_run.command)
-        base_run.command = re.sub(r'{NUM_DOMAINS}', args.num_domains,
+        base_run.command = re.sub(r'{NUM_DOMAINS}', str(args.num_domains),
+                                  base_run.command)
+        base_run.command = re.sub(r'{MODEL_SIZE}', str(args.model_size),
+                                  base_run.command)
+        base_run.command = re.sub(r'{SPLIT}', args.split, base_run.command)
+        base_run.command = re.sub(r'{REF_MODEL_CKPT}', args.ref_model_ckpt,
+                                  base_run.command)
+        base_run.command = re.sub(r'{REMOTE_BASE}', args.remote_base,
+                                  base_run.command)
+        base_run.command = re.sub(r'{UPLOAD_DIR}', args.upload_dir,
                                   base_run.command)
 
         launched_run = create_run(base_run)
