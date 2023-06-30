6856f232198d2595b782028f36febc17dca4f084
Zack Ankner <zackankner@gmail.com>
2023-06-30 10:21:30 -0700

    Refactor get_sample_with_uid function to get_sample_int_keys

--- do-remb-mi/scripts/data/utils.py
+++ do-remb-mi/scripts/data/utils.py
@@ -1,5 +1,6 @@
-
-def get_sample_with_uid(sample):
-    uid = sample["uid"].item()
-    del sample["uid"]
-    return {**sample, "uid": uid}
+def get_sample_int_keys(sample, int_keys):
+    int_keys_unpacked = {}
+    for int_key in int_keys:
+        int_keys_unpacked[int_key] = sample[int_key].item()
+        del sample[int_key]
+    return {**sample, **int_keys_unpacked}
