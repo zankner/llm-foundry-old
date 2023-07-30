f4007e1f9793fa63d943860f99bc573d00b85d84
Zack Ankner <zackankner@gmail.com>
2023-07-30 06:02:07 +0000

    merged models

--- brick/merge_models.py
+++ brick/merge_models.py
@@ -3,40 +3,15 @@ import os
 import tempfile
 
 import torch
-import pathlib
-import shutil
-import uuid
 from composer.utils import get_file
-from composer.loggers import RemoteUploaderDownloader as ComposerRemoteUploaderDownloader
 
-
-class RemoteUploaderDownloader(ComposerRemoteUploaderDownloader):
-
-    def upload_file(
-        self,
-        remote_file_name: str,
-        file_path: pathlib.Path,
-        *,
-        overwrite: bool,
-    ):
-        copied_path = os.path.join(self._upload_staging_folder,
-                                   str(uuid.uuid4()))
-        os.makedirs(self._upload_staging_folder, exist_ok=True)
-        shutil.copy2(file_path, copied_path)
-        formatted_remote_file_name = self._remote_file_name(remote_file_name)
-        with self._object_lock:
-            if formatted_remote_file_name in self._logged_objects and not overwrite:
-                raise FileExistsError(
-                    f'Object {formatted_remote_file_name} was already enqueued to be uploaded, but overwrite=False.'
-                )
-            self._logged_objects[formatted_remote_file_name] = (copied_path,
-                                                                overwrite)
+from ckpt_utils import RemoteUploaderDownloader
 
 
 def load_state_dict(model_ckpt: str):
     with tempfile.NamedTemporaryFile() as tmp:
         get_file(model_ckpt, tmp.name, overwrite=True)
-        state_dict = torch.load(tmp.name)
+        state_dict = torch.load(tmp.name, map_location="cpu")
     return state_dict
 
 
