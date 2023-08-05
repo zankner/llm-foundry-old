b95ff27edff8e2dabf16e1f10a93c9d7d03a495b
Zack Ankner <zackankner@gmail.com>
2023-08-04 20:07:15 -0700

    Add get_run_name and slack_logger files

---
+++ amortized-obs/get_run_name.py
@@ -0,0 +1,91 @@
+import argparse
+from pretrain_utils import (build_ref_base, build_proxy_base, build_final_base)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # Reference args
+    parser.add_argument("--ref-model-size", type=str, choices=["125M", "250M"])
+    parser.add_argument("--ref-num-tokens",
+                        type=str,
+                        choices=["2B", "5B", "20B", "26B"])
+
+    # Proxy args
+    parser.add_argument("--proxy-model-size",
+                        type=str,
+                        choices=["125M", "250M", "1B"])
+    parser.add_argument("--proxy-num-tokens",
+                        type=str,
+                        choices=["2B", "5B", "20B", "26B"])
+    parser.add_argument(
+        "--full-batch-size",
+        help="Batch size for points to be labeled that will then be pruned",
+        type=int,
+        choices=[1024, 2048, 4096])
+    parser.add_argument("--num-pplx-filter", type=int, default=0)
+    parser.add_argument("--selection-algo",
+                        type=str,
+                        choices=["rho", "hard-mine", "easy-mine", "baseline"
+                                ])  # Treat baseline as a selection algo
+
+    # Final args
+    parser.add_argument("--final-model-size",
+                        type=str,
+                        choices=["125M", "250M", "1B"])
+    parser.add_argument("--final-num-tokens",
+                        type=str,
+                        choices=["2B", "5B", "20B", "26B"])
+
+    # Data args
+    parser.add_argument("--dataset", type=str, default="pile", choices=["pile"])
+    parser.add_argument("--device-batch-size", type=int, default=32)
+    parser.add_argument("--holdout-num-tokens",
+                        type=str,
+                        required=True,
+                        choices=["2B", "5B", "20B", "26B"])
+
+    # Misc
+    parser.add_argument("--run-type",
+                        type=str,
+                        required=True,
+                        choices=["reference", "proxy", "final"])
+
+    # Seed args
+    parser.add_argument("--seed", type=int, required=True)
+
+    args = parser.parse_args()
+
+    # Building the run name
+    if args.run_type == "final":
+        final_base = build_final_base(args.final_num_tokens,
+                                      args.final_model_size)
+        if args.selection_algo == "baseline":
+            run_name = f"final-{args.dataset}-baseline-{final_base}"
+        else:
+            proxy_run_base = build_proxy_base(
+                args.selection_algo, args.proxy_num_tokens,
+                args.proxy_model_size, args.full_batch_size,
+                args.num_pplx_filter, args.ref_num_tokens, args.ref_model_size)
+            fmt_proxy_run_base = proxy_run_base.replace(
+                f"{args.selection_algo}-", "")
+            run_name = f"final-{args.dataset}-{args.selection_algo}-{final_base}-{fmt_proxy_run_base}"
+    elif args.run_type == "proxy":
+        model_size = args.proxy_model_size
+        num_tokens = args.proxy_num_tokens
+        proxy_run_base = build_proxy_base(
+            args.selection_algo, args.proxy_num_tokens, args.proxy_model_size,
+            args.full_batch_size, args.num_pplx_filter, args.ref_num_tokens,
+            args.ref_model_size)
+        run_name = f"proxy-{args.dataset}-{proxy_run_base}"
+    elif args.run_type == "reference":
+        ref_base = build_ref_base(args.ref_num_tokens, args.ref_model_size)
+        run_name = f"ref-{args.dataset}-{ref_base}"
+
+    run_name = f"{run_name}-holdt-{args.holdout_num_tokens}-sd-{args.seed}"
+    print("Train run name:")
+    print(run_name)
+    print()
+    print("Eval final name:")
+    print(f"eval-final-{run_name}")
+    print()
+    print("Eval sweep name:")
+    print(f"eval-sweep-{run_name}")
---
+++ llmfoundry/callbacks/slack_logger.py
@@ -0,0 +1,40 @@
+import os
+import logging
+
+from composer import Callback, Logger, State
+from composer.utils import MissingConditionalImportError
+
+log = logging.getLogger(__name__)
+
+
+class SlackLogger(Callback):
+
+    def __init__(self):
+        try:
+            import slack_sdk
+            self.client = slack_sdk.WebClient()
+            del slack_sdk
+        except ImportError as e:
+            raise MissingConditionalImportError('slack_logger', 'slack_sdk',
+                                                None) from e
+        self.slack_logging_api_key = os.environ.get('SLACK_LOGGING_API_KEY')
+        self.channel_id = os.environ.get('SLACK_LOGGING_CHANNEL_ID', None)
+
+    def fit_end(self, state: State, logger: Logger):
+        message = f"QUICK THE TOKEN TRAIN IS LEAVING THE STATION\nRun finished: {state.run_name}"
+        try:
+            self.client.chat_postMessage(token=self.slack_logging_api_key,
+                                         channel=self.channel_id,
+                                         text=message)
+        except Exception as e:
+            log.error(f'Error logging to Slack: {e}')
+
+
+if __name__ == "__main__":
+
+    class TestState(object):
+        run_name = 'test_run'
+
+    test_state = TestState()
+    slack_logger = SlackLogger()
+    slack_logger.fit_end(test_state, None)
